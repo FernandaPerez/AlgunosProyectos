e9a43a590f57730ba9d124b0a1663c848631e054
FernandaPerez <marifer_92@ciencias.unam.mx>
2015-04-14 01:51:47 -0500

    ya hay grafica

--- tarea_examen.py
+++ tarea_examen.py
@@ -2,27 +2,31 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from scipy import integrate, special
+from matplotlib import cm, colors
 
 #Condiciones Iniciales
 R0 = 1.
 V0 = 5.
 
-#Definimos las funciones especiales que necesitaremos
-def Plm (grado, orden, x):
+#Definimosx las funciones especiales que necesitaremos
+def Plm (x, grado, orden):
     return special.lpmv(orden, grado, x)
 def Ylm (grado, orden, th, phi):
     return special.sph_harm(orden, grado, phi, th)
-
 #Ahora los coeficientes Alm:
 def Alm (grado, orden):
-    integral = integrate.quad(Plm, -1., 1., args = (grado, orden))
-    return np.sqrt(5. / (np.pi * 4. * np.factorial(4))) * (1./(R0 ** grado)) * integral
+    integral = integrate.quad(Plm, 1., -1., args = (grado, orden))
+    if integral <= 1e-8:
+        integral = 0
+    a =  np.sqrt(5. / (np.pi * 4. * np.math.factorial(4))) * (1./(R0 ** grado)) * integral[0]*complex(0, (8*V0 / orden))
 
+    print a
+    return a
 #Grados del polinomio que estamos dispuestos a sumar
 M = 4
 
 #Iteramos, seleccionando los ordenes validos
-def V (r, th, phi, M=4):
+def V (r, th, phi, M=10):
     
     V = 0.
     for l in range (1, M+1):
@@ -32,22 +36,27 @@ def V (r, th, phi, M=4):
                 lista.append(m)
 
         for m_valida in lista:
-            V = V + Alm (l, m_valida)*(1./(r**grado))*Ylm(l, m_valida, th, phi)
+            V =  V + Alm (l, m_valida)*Ylm(l, m_valida, th, phi)
     return V
         
-pasos = 100
+pasos = 150
 
-th = np.linspace(0. , np.pi, pasos)
+th = np.linspace(0. , 2*np.pi, pasos)
 rr = np.linspace(0., R0, pasos)
 
 R, Th = np.meshgrid(rr, th)
-
+Ph = np.ones_like(R) * (np.pi / 4.)
 X = R*np.cos(Th)
 Y = R*np.sin(Th)
 
+color_map= np.real(V(R,Th,Ph))
+
 fig = plt.figure()
 ax = plt.axes()
-im = ax.scatter(X, Y)
+ax.set_title(r"Grafica para $V(r, \theta, \phi = \frac{\pi}{2})$")
+im = ax.scatter(X, Y, c=color_map)
+plt.colorbar(im, shrink=0.7)
+
 
 plt.show()
     
